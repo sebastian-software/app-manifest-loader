bb12d4ad0969093165c2924b716d542a2f995a73
Sebastian Werner <s.werner@sebastian-software.de>
2018-08-16 13:01:26 +0200

    Added complex sub folder + public path config for browserconfig

---
+++ test/success-cases/complex-browserconfig/webpack.config.js
@@ -0,0 +1,48 @@
+export default {
+  entry: {
+    main: `${__dirname}/index.js`
+  },
+
+  context: __dirname,
+  mode: "development",
+  devtool: false,
+
+  output: {
+    filename: "index.js",
+    path: `${__dirname}/actual-output`,
+    publicPath: "http://cdn.example.com/assets/"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /browserconfig\.xml$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "browserconfig.xml"
+            }
+          },
+          {
+            loader: "../../../",
+            options: {
+              publicPath: "http://cdn.example.com/assets/"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.gif$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "gifs/file-[name].[ext]"
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
